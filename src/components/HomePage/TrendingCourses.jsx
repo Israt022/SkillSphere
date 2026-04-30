050831837ce6e7170b61e7cdfff97781a7d0a5aa
Israt Jahan <eraisrat022@gmail.com>
2026-05-01 03:28:54 +0600

    fix

--- src/components/HomePage/TrendingCourses.jsx
+++ src/components/HomePage/TrendingCourses.jsx
@@ -12,7 +12,7 @@ const TrendingCourses = ({ courses }) => {
     <div className="max-w-7xl mx-auto py-10">
 
       <h2 className="text-3xl font-bold text-center mb-6">
-        🔥 Trending Courses
+         Trending Courses
       </h2>
 
       <Swiper
@@ -20,7 +20,7 @@ const TrendingCourses = ({ courses }) => {
         spaceBetween={20}
         slidesPerView={3}
         loop={true}
-        speed={800}   // 👉 smooth movement
+        speed={800}  
         autoplay={{
           delay: 2000,
           disableOnInteraction: false,
