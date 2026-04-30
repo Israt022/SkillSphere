9b70491801153b8a9e3a0738de6eb4de19d05dd6
Israt Jahan <eraisrat022@gmail.com>
2026-04-30 23:45:10 +0600

    Added Update Profile

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import UpdateUserModal from "@/components/UpdateUserModal";
+import { authClient } from "@/lib/auth-client";
+import { Button, Card } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const ProfilePage = () => {
+  const userData = authClient.useSession();
+  const user = userData.data?.user;
+  return (
+    <div className="container mx-auto pt-20">
+      <Card className="p-8 shadow-md text-center w-8/12 mx-auto border border-indigo-500/50 shadow-indigo-200">
+        {/* Avatar */}
+        <div className="w-32 h-32 mx-auto relative">
+          <Image
+            src={user?.image}
+            alt={user?.name}
+            fill
+            className="rounded-full object-cover"
+          />
+        </div>
+
+        {/* Info */}
+        <h2 className="text-2xl font-bold mt-4">{user.name}</h2>
+
+        <p className="text-gray-500">{user.email}</p>
+
+        {/* Button */}
+        <div className="mt-6">
+            <UpdateUserModal/>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default ProfilePage;
